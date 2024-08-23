7fa450941d34db629cced66a4b873879f5ff470b
aishvarya <aishvarya@trafyai.com>
2024-08-23 09:46:34 +0000

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import BlogLanding from "@/components/blog-page/blog-landing/BlogLanding";
+
+export default function Home() {
+  return (
+   <div>
+      <BlogLanding/>
+   </div>
+   
+  );
+}
---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+// src/firebase.js
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database'; // Use getDatabase for Realtime Database
+import { getStorage } from 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqL7lnooyjmNGAOB5nc4yZcb6FKu8e-2A",
+  authDomain: "trafyai-loginsignup.firebaseapp.com",
+  projectId: "trafyai-loginsignup",
+  storageBucket: "trafyai-loginsignup.appspot.com",
+  messagingSenderId: "344792634329",
+  appId: "1:344792634329:web:d343ac2461dd2a731dffc8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const auth = getAuth(app);
+const database = getDatabase(app); // For Realtime Database
+const storage = getStorage(app);
+
+
+export { auth, database, storage };
