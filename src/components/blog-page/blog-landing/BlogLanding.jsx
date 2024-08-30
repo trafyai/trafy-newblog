6b31ab726ee1f00e81b93d63ec3043f9d78d30be
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-08-30 16:26:36 +0530

    hii

--- src/components/blog-page/blog-landing/BlogLanding.jsx
+++ src/components/blog-page/blog-landing/BlogLanding.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import '@/styles/blog/BlogLanding.css'
+import '@/styles/blog/BlogLanding.css';
 import BlogSingleData from "@/api/blog/BlogSingleData";
 import Link from "next/link";
 import Image from "next/image";
 
-
-export default function BlogLanding(){
-
-    return(
+export default function BlogLanding() {
+    return (
         <main>
             <div className="blog-landing-page">
                 <div className="blog-landing-container">
 
-                    {BlogSingleData.slice(0,1).map((item,index)=>(
-                    
-                        <Link href={`/${item.id}`} className="blog-landing-banner">
+                    {BlogSingleData.slice(0, 1).map((item) => (
+                        <Link key={item.id} href={`/${item.id}`} className="blog-landing-banner">
                             <div className="blog-landing-banner-image">
                                 <Image src={item.image} alt={item.alt} />
                             </div>
@@ -28,46 +25,39 @@ export default function BlogLanding(){
                                 </div>
                             </div>
                         </Link>
-                    )
-                    )}
+                    ))}
 
                     <div className="blog-landing-content">
-
-                        {BlogSingleData.slice(1, 4).map((item,index)=>
-                    
-                        <div className="blog-landing-card" key={index}>
-                            
-                            <Link href={`/${item.id}`}  className="blog-landing-card-inner">
-                            <div className="blog-landing-card-image" >
-                                <Image src={item.image} alt={item.alt} height="max-content"/>
+                        {BlogSingleData.slice(1, 4).map((item) => (
+                            <div className="blog-landing-card" key={item.id}>
+                                <Link href={`/${item.id}`} className="blog-landing-card-inner">
+                                    <div className="blog-landing-card-image">
+                                        <Image src={item.image} alt={item.alt} height="max-content" />
+                                    </div>
+                                    <div className="blog-landing-card-content">
+                                        {/* <div className="blog-landing-card-category">
+                                            <h1>{item.category}</h1>
+                                            <p>{item.read}</p>
+                                        </div> */}
+                                        <div className="blog-landing-card-heading">
+                                            <h1>{item.title}</h1>
+                                        </div>
+                                        <div className="blog-landing-card-description">
+                                            <p>
+                                                {String(item.description).slice(0, 130)}...
+                                            </p>
+                                        </div>
+                                        <div className="blog-landing-card-author">
+                                            <p>{item.author}</p>
+                                            <p>{item.date}</p>
+                                        </div>
+                                    </div>
+                                </Link>
                             </div>
-                            <div className="blog-landing-card-content">
-                            {/* <div className="blog-landing-card-category">
-                                <h1>{item.category}</h1>
-                                <p>{item.read}</p>
-                            </div> */}
-                            <div className="blog-landing-card-heading">
-                                <h1>{item.title}</h1>
-                            </div>
-                            <div className="blog-landing-card-description">
-                            <p>
-                            {/* {item.description} */}
-
-                            {String(item.description).slice(0, 130)}...
-                            </p>
-                            </div>
-                            <div className="blog-landing-card-author">
-                                <p>{item.author}</p>
-                                <p>{item.date}</p>
-                            </div>
-                            </div>
-                            </Link>
-                        </div>
-                        
-                        )}
+                        ))}
                     </div>    
                 </div>
             </div>
         </main>
-    )
+    );
 }
